bce17dda2981098a21c9a58f3f73e15cba2e0153
heathercoraje <yhk7830@hotmail.com>
2018-01-18 00:35:33 +0900

    add button

--- index.js
+++ index.js
@@ -8,15 +8,24 @@ var content = [
   },
   { quote:"“Most people quit because they look how far they have to go, not how far they have come.”",
     author: "Ziad K. Abdelnour"
+  },
+  { quote:"“One of the best programming skills you can have is knowing when to walk away for awhile.”",
+    author: "Oscar Godson"
+  },
+  { quote:"“Quitting is never an option.”",
+    author: "Ana Ivanovic"
+  },
+  { quote:"“Quantity and persistence will get you the outcomes you need.”",
+    author: "James Altucher"
   }
 ];
 
 (function getQuote() {
-  var time = new Date().getHours();
+  var hour = new Date().getHours();
   var greeting = document.querySelector('#greeting');
   var quote = document.querySelector('#quote');
   var author = document.querySelector('#author');
-  var index = Math.floor(Math.random() * Math.floor(3));
+  var index = Math.floor(Math.random() * Math.floor(content.length));
   var next = document.querySelector('#next');
   var tweet = document.querySelector('#tweet');
   var shareTweet = function(obj) {
@@ -25,8 +34,7 @@ var content = [
     console.log(link);
     tweet.href = link;
   }
-
-  greeting.innerText = (time<12)? 'Good morning':'Good afternoon';
+greeting.innerText = (hour < 12)? 'Good morning':'Good afternoon !' ;
   quote.innerText= content[index].quote;
   author.innerHTML= content[index].author;
   setTimeout(getQuote, 10000); // invoke every 10 sec
